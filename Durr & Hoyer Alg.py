7cb2da4b592d56134305698f674c9e6938e07597
mridulsar <mridul1399@gmail.com>
2020-06-01 10:50:11 -0700

    New Alg
    Removed Microsoft Grover
    Need to implement my own grovers, may us QisKit alg for grovers

--- Durr & Hoyer Alg.py
+++ Durr & Hoyer Alg.py
@@ -1,29 +1,33 @@
 from pennylane import numpy as np
 import qsharp
-from SimpleGrover import SearchForMarkedInput
-def Durr_Hoyer(table):
-    table = np.ndarray.flatten(table)
-    N = np.size(table)
-    y = np.random.uniform(0,N-1)
-    quantum_reg= np.zeros(N)
-    marked = np.zeros(N)
+import pennylane as qml
+class Durr_Hoyer:
+    def ___init___(self,table):
+        self.table = np.ndarray.flatten(table)
+        self.N = np.size(table)
+        self.y = np.random.uniform(0,self.N-1)
+        self.states = 
 
-    dev1 = qml.device("default.qubit",wire=N)
-    
-    while time.clock()<(22.5*sqrt(N)+1.4*log(N)^2):
-        for i in [1:N]:
-            quantum_reg[i]=(qml.qnodes.QubitQNode(i,dev1(wire=i))
-            quantum_reg[i]=1/sqrt(N)*qml.qnodes.QubitQNode(quantum_reg[y],dev1(wire=i))*quantum_reg[i]
-            if table[i] < table[y]:
-                marked[i]=1
-                continue
+    def circut(self,wire_num):
+            for num in range(self.wire_num):
+                if table[num]<table[y]:
+                    qml.BasisState(n=states[num] ,wires=num)
+                    return
+                else:
+                    self.table.pop(num)
+                    self.wire_num = self.wire_num - 1
+                    continue
+            basis = 1/np.sqrt(N) .* np.ones(self.wire_num)
+            basis = expval(qml.Identity(y)) .* basis
+            qml.broadcast(basis,wires=range(self.wire_num))
+            #index=Grovers Alg
+            return index
+    dev1 = qml.device("default.qubit",wires=self.N)
+    def algorithmI(self,N,y)
+        while (time.clock()<(22.5*sqrt(N)+1.4*log(N)^2)):
+            y_prime=circuit(self.N)
+            if table(y_prime) < table(self.y):
+                return y_prime
             else:
-                continue
-        quantum_reg=np.transpose(marked) * quantum_reg
-        
-        #modify grovers so that m=1 phi=6/5 phi can be between (1,4/3)
-        y_prime=SearchForMarkedInput(quantum_reg)
-        #if T[y_prime = min(T)] continue otherwise do grovers again, implement this into grover
-
-        if table(y_prime) < table(y):
-            return y_prime
+                return self.y
+        return self.y
