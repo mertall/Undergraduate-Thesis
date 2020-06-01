224cd4a6e62734899127d1b2a7e99d4688731e45
mridulsar <mridul1399@gmail.com>
2020-06-01 15:04:21 -0700

    fixed qubit basis and improved big(O)

--- Durr & Hoyer Alg.py
+++ Durr & Hoyer Alg.py
@@ -6,34 +6,54 @@ import Grovers as gr
 # Only works for 4 element table
 class Durr_Hoyer:
     def ___init___(self,table):
-        self.table = np.ndarray.flatten(table)
-        self.m,self.n = np.size(table)
-        self.N = self.m*self.n
-        self.y = np.random.uniform(0,self.N-1)
-        self.states = [[10],[01],[11],[00]]
-        dev1 = qml.device("default.qubit",wires=self.N)
+        self.table = np.ndarray.flatten(table)                  #Vectorize table
+        n = len(self.table)                                     #Grab dimensions
+        self.N = n - 1                                          #...
+        self.y = np.random.uniform(0,self.N-1)                  #Threshold Index
 
-        @qml.qnode(dev1)
-    def create_states(self,N)
-        #
-    def compare(self,s,t)
-        return sorted(self.s) >= self.t
-    def circuit(self,wire_num):
-            marked=compare(table,table[y])
-            self.states = self.states - (marked .* self.states)
-            qml.BasisState(n=self.states,wires=self.wire_num)
-            basis = 1/np.sqrt(N) .* np.ones(self.wire_num)
-            basis = qml.expval(qml.Identity(y)) .* basis
-            qml.broadcast(basis,wires=range(self.wire_num))
-            qubits = qml.Identity(range(N))
-            grovers = gr.calculate(qubits,self.wire_num)
-            return qml.expval(qml.Identity(0))
+    def compare(self,s,t,N)
+        return sorted(self.s) > (self.t .* np.ones(self.N))
 
-    def algorithm(self,N,y):
-        while (time.clock()<(22.5*sqrt(self.N)+1.4*log(self.N)^2)):
-            y_prime=circuit(self.N)
-            if table(y_prime) < table(self.y):
-                return y_prime
-            else:
-                return self.y
-        return self.y
+    def circuit(self,N,y,wire_num):
+
+            wire=self.wire_num
+            N = self.N
+            y= self.y
+
+            qml.broadcast(hadmard,range(wire))                  #Create a unique set of qubits based on wire number
+            
+            basis = 1/np.sqrt(N) .* np.ones(wire)               #Modify Basis as mentioned in paper
+            basis = qml.expval(qml.Identity(self.y)) .* basis   #...
+            qml.broadcast(basis,wires=range(wire))              #...
+            
+            qubits = qml.Identity(range(wire))                  #Grab state of qubits for Grovers
+            
+            grovers = gr.calculate(qubits,wire)                 #Apply Grovers
+            
+            return qml.expval(qml.Identity(0))                  #Grab first register as stated by paper
+
+    def algorithm(self,N,y,table):
+
+        while (time.clock()<(22.5*sqrt(self.N)+1.4*log(self.N)^2)):      #Run until time stated by paper
+            
+            t=self.table
+            index =[]
+
+            markers=compare(self.table,self.table[y],self.N)                    #Compare T[j]<T[y] for all j in N
+            
+            self.wire_num = len(markers)                                 #Wires for qubits based on compare()
+            
+            t = t - (t .* markers)                                       #Hot enconde all values of the table that were greater than T[y] as 0
+
+            t,index= [t[i],i for i in t if t[i] != 0]                    #Remove all values that are zero and record indices that are non-zero
+            
+            dev = qml.device("default.qubit",wires=self.wire_num)        #Set up quantum device and run circuit
+            @qml.qnode(dev)
+            y_prime=circuit(self.N,self.y,t,self.wire_num)               #Evaluate Quantum circuit to find y_prime
+            
+            if self.table(index(y_prime)) < self.table(self.y):
+                return index(y_prime)                                      #Return index for minimum (Probability of success: 1/2)
+            else:                                                          #...
+                return self.y                                              #...
+        
+        return self.y                                                      #...
--- Grover_Alg.py
+++ Grover_Alg.py
@@ -2,21 +2,26 @@ import pennylane as qml
 from pennylane import numpy as np 
 #https://arxiv.org/pdf/quant-ph/9605034.pdf
 class Grovers:
+    
     def ___init___(self,qubits,N)
         self.qubits=qubits
         self.N=N
-    def S(self,qubit,N): #Conditional Phase Shift Transform
+    
+    def S(self,qubit,N):                                    #Conditional Phase Shift Transform
         if self.qubits is in list((range(2^N*10^N) % N)):
             self.qubits = -self.qubits
+        
         else 
             self.qubits = self.qubits
+        
         return self.qubits
 
 
-    def T_t(self,qubits,N) #Transpose of Walsh-Hadamard transform
+    def T_t(self,qubits,N)                                  #Transpose of Walsh-Hadamard transform
 
         #self.qubit=1/sqrt(N)sum(i...N-1)|i\*transpose(|0\)
         reutrn self.qubits
 
-    def calculate(self,qubits,N)
+    def calculate(self,qubits,N)                            #Grovers Algorithm
+        
         return T_t(self.qubits,self.N))*S(self.qubits,0)*1/T_t(self.qubits.self.N)*S(self.qubits,self.N)
