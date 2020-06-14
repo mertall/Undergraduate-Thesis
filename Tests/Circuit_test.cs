baf07a078304f61fe84624db99988974954ac9a8
mridulsar <mridul1399@gmail.com>
2020-06-13 21:40:41 -0700

    Update Circuit_test.cs

--- Tests/Circuit_test.cs
+++ Tests/Circuit_test.cs
@@ -1,21 +1,27 @@
 // Written By Mridul Sarkar
 // 6/6/20 2:43 AM
 
-using (var sim = new QuantumSimulator(randomNumberGenerator(42)))
-{
-    var estimated_T = T_t.Run(sim).Result;
-    var exact_T = Walsh_Hadmard.Run(sim).Result;
-    var cond_phase_shift = S_A.Run(sim).Result;
-    var cond_phase_shift0 = S_0.Run(sim).Result;
+using System;
 
-    if (estimated_T == exact_T) {
+static Boolean Main() {
 
-        return True
-    }
-    else {
-        return False
-    }
+
+    using (var sim = new QuantumSimulator(randomNumberGenerator(42)))
+    {
+        var estimated_T = T_t.Run(sim).Result;
+        var exact_T = Walsh_Hadmard.Run(sim).Result;
+        var cond_phase_shift = S_A.Run(sim).Result;
+        var cond_phase_shift0 = S_0.Run(sim).Result;
+
+        if (estimated_T == exact_T) {
+
+            return True
+        }
+        else {
+            return False
+        }
     // return (exact_T*exact_T_transpose) // Find out how to use alt and control = inverse feature to invert this gate
     // return (cond_phase_shift * cond_phase_shift_transpose) // ...
     // return (cond_phase_shift0 * cond_phase_shift0_transpose) 
+    }
 }
